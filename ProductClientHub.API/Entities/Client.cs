7300c7ac5271d4d2fed4cce13e609872e995e919
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-04 17:19:14 -0300

    Banco de Dados e criação das Entidades

--- ProductClientHub.API/Entities/Client.cs
+++ ProductClientHub.API/Entities/Client.cs
@@ -2,5 +2,8 @@
 {
     public class Client
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
