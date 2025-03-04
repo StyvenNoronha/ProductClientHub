7ead1058dc0ccb05f056bbae60b9ca761606ba78
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-04 18:01:18 -0300

    Salvando registro no Banco de Dados

--- ProductClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
+++ ProductClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -1,4 +1,6 @@
-﻿using ProductClientHub.Communication.Request;
+﻿using ProductClientHub.API.Entities;
+using ProductClientHub.API.Infraestructure;
+using ProductClientHub.Communication.Request;
 using ProductClientHub.Communication.Response;
 using ProductClientHub.Exceptions.ExcptionsBase;
 
@@ -8,14 +10,33 @@ namespace ProductClientHub.API.UseCases.Clients.Register
     {
         public ResponseClientJson Execute(RequestClientJson request)
         {
+
+            Validate(request);
+            var dbContext = new ProductClientHubDbContext();
+
+            var entity = new Client
+            {
+                Name = request.Name,
+                Email = request.Email,
+                
+            };
+
+            dbContext.Clients.Add(entity);
+            dbContext.SaveChanges();
+            return new ResponseClientJson();
+        }
+
+        private void Validate(RequestClientJson request)
+        {
             var validator = new RegisterClientValidator();
+
             var result = validator.Validate(request);
+
             if (result.IsValid == false)
             {
                 var errors = result.Errors.Select(failure => failure.ErrorMessage).ToList();
-              
+
             }
-            return new ResponseClientJson();
         }
     }
 }
