eada6bcab2193a5f5a940ed6e6f8d194dbd64e5e
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-04 08:11:15 -0300

    Validando dados

---
+++ ProductClientHub.API/UseCases/Clients/Register/RegisterClientValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ProductClientHub.Communication.Request;
+
+namespace ProductClientHub.API.UseCases.Clients.Register
+{
+    public class RegisterClientValidator : AbstractValidator<RequestClientJson>
+    {
+        public RegisterClientValidator()
+        {
+            RuleFor(client => client.Name).NotEmpty().WithMessage("o Nome não pode ser vazio");
+            RuleFor(client => client.Email).EmailAddress().WithMessage("o Email não pode ser vazio");
+        }
+    }
+}
