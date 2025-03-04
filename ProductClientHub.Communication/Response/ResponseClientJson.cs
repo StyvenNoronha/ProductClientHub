ed5297639669edd58e03c6641416bffac52d9fd5
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-04 07:38:31 -0300

    Preparando o endpoint para registrar clientes

---
+++ ProductClientHub.Communication/Response/ResponseClientJson.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace ProductClientHub.Communication.Response
+{
+    public class ResponseClientJson
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty; 
+    }
+}
